aea8d265bc14dd3aeddd4e2cca04685c270024af
Wisdom <eluewisdom@gmail.com>
2022-11-27 16:29:41 +0100

    feat: adding sanitization setting headers body parser and other libraries utilization

--- dist/app.js
+++ dist/app.js
@@ -29,6 +29,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importStar(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+const xss_clean_1 = __importDefault(require("xss-clean"));
+const helmet_1 = __importDefault(require("helmet"));
+const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const globalError_1 = require("./utils/globalError");
+const propertyRoutes_1 = __importDefault(require("./routes/propertyRoutes"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 exports.app = (0, express_1.Router)();
 exports.app.use((0, cors_1.default)());
 exports.app.use(express_1.default.json({ limit: "10kb" }));
+exports.app.use((0, cookie_parser_1.default)());
+exports.app.use((0, xss_clean_1.default)());
+exports.app.use((0, express_mongo_sanitize_1.default)());
+exports.app.use((0, helmet_1.default)());
+exports.app.use((req, res, next) => {
+    next();
+});
+const limiter = (0, express_rate_limit_1.default)({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP. Please try again in an hour.",
+});
+if (process.env.NODE_ENV === "development") {
+    exports.app.use((0, morgan_1.default)("dev"));
+}
+exports.app.use("/api/v1/users", userRoutes_1.default);
+exports.app.use("/api/v1/properties", propertyRoutes_1.default);
+exports.app.all("*", (req, res, next) => {
+    next(new globalError_1.GlobalError(`Oops! Can't find ${req.originalUrl} on this server`, 404));
+});
