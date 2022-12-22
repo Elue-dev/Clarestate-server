2da8db6bd2c09257de925468a084f8b163c7c873
Wisdom <eluewisdom@gmail.com>
2022-12-22 13:04:57 +0100

    feat: token handler function

---
+++ dist/service/auth_service.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createAndSendToken = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const generateToken = (id) => {
+    return jsonwebtoken_1.default.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_COOKIE_EXPIRES_IN,
+    });
+};
+const createAndSendToken = (user, statusCode, res) => {
+    const token = generateToken(user._id);
+    const cookieOptions = {
+        expires: new Date(
+        //@ts-ignore
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        path: "/",
+        httpOnly: true,
+        secure: false,
+    };
+    if (process.env.NODE_ENV === "production")
+        cookieOptions.secure = true;
+    res.cookie("token", token, cookieOptions);
+    res.status(statusCode).json({
+        status: "success",
+        token,
+        data: user,
+    });
+};
+exports.createAndSendToken = createAndSendToken;
---
+++ server/service/auth_service.ts
@@ -0,0 +1,36 @@
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET as string, {
+    expiresIn: process.env.JWT_COOKIE_EXPIRES_IN,
+  });
+};
+
+export const createAndSendToken = (
+  user: any,
+  statusCode: number,
+  res: Response
+) => {
+  const token = generateToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      //@ts-ignore
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    path: "/",
+    httpOnly: true,
+    secure: false,
+  };
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("token", token, cookieOptions);
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: user,
+  });
+};
