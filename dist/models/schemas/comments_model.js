77be2cc3619d8830e9d644c56aeac937af80cdb8
Wisdom <eluewisdom@gmail.com>
2022-12-24 22:02:33 +0100

    feat: comments model

--- dist/models/schemas/comments_model.js
+++ dist/models/schemas/comments_model.js
@@ -4,6 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const commentSchema = new mongoose_1.default.Schema({});
+const commentSchema = new mongoose_1.default.Schema({
+    comment: {
+        type: String,
+        required: [true, "Your comment is required"],
+    },
+    property: {
+        type: mongoose_1.default.Schema.Types.ObjectId,
+        ref: "Tour",
+        required: [true, "A comment must belong to a property"],
+    },
+    user: {
+        type: mongoose_1.default.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "A comment must belong to a user"],
+    },
+});
 const Comment = mongoose_1.default.model("comment", commentSchema);
 exports.default = Comment;
--- server/models/schemas/comments_model.ts
+++ server/models/schemas/comments_model.ts
@@ -1,6 +1,21 @@
 import mongoose from "mongoose";
 
-const commentSchema = new mongoose.Schema({});
+const commentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: [true, "Your comment is required"],
+  },
+  property: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Tour",
+    required: [true, "A comment must belong to a property"],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "A comment must belong to a user"],
+  },
+});
 
 const Comment = mongoose.model("comment", commentSchema);
 
